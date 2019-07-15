07487252fe435b7ab9c4be63b3f3d0bb34164539
Matthew-Steen <steen-m@ulster.ac.uk>
2019-07-15 16:26:12 +0100

    Add basic HTML table to display jobs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'; 
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { CareerTableComponent } from './career-table/career-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CareerTableComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/career-table/career-table.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-career-table',
+  templateUrl: './career-table.component.html',
+  styleUrls: ['./career-table.component.css']
+})
+export class CareerTableComponent implements OnInit {
+  // departmentID is passed from previous page in order to know what department data to show
+  @Input() departmentID;
+  
+  // Roles is filled by data service
+    // roles:any[];
+  roles = [
+    {
+      "name": "Head of Business Unit",
+      "band": "Executive",
+      "capability": "Business Development"
+    },
+    {
+      "name": "Sales Director",
+      "band": "Leadership Community",
+      "capability": "Business Development"
+    },
+    {
+      "name": "Business Development Director",
+      "band": "Principal",
+      "capability": "Business Development"
+    },
+    {
+      "name": "Senior Sales Executive",
+      "band": "Principal",
+      "capability": "Sales"
+    }
+  ];
+
+  bands = [
+    {
+      "name": "Executive"
+    },
+    {
+      "name": "Leadership Community"
+    },
+    {
+      "name": "Principal"
+    },
+    {
+      "name": "Manager"
+    },
+    {
+      "name": "Consultant"
+    },
+    {
+      "name": "Senior Associate"
+    },
+    {
+      "name": "Associate"
+    },
+    {
+      "name": "Trainee"
+    },
+    {
+      "name": "Apprentice"
+    }
+  ]
+
+  capabilities = [
+    {
+      "name": "Business Development"
+    },
+    {
+      "name": "Account Management"
+    },
+    {
+      "name": "Sales"
+    },
+    {
+      "name": "Inside Sales Development"
+    },
+    {
+      "name": "Pre Sales & Bid Management"
+    },
+    {
+      "name": "Marketing"
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+    //call endpoint
+  }
+
+
+  
+}
