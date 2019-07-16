7cf5d667b96cd410d05d6174e9d1789777daddaf
Marc Templeton <marcte@nighty.local>
2019-07-16 10:46:44 +0100

    navbar created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,26 @@
 
-<<<<<<< HEAD
 import { BrowserModule } from "@angular/platform-browser";
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { DataService } from "./_services/data.service";
 import { AppComponent } from "./app.component";
 import { CareerTableComponent } from './career-table/career-table.component';
-
-@NgModule({
-  declarations: [AppComponent, CareerTableComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [DataService],
-=======
-import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    NavbarComponent,
+    CareerTableComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule
   ],
-  providers: [],
->>>>>>> landing page component
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
+
 export class AppModule {}
