16690289d770bbbba5159bd66f3b754fa8a3597e
Matthew-Steen <steen-m@ulster.ac.uk>
2019-07-16 12:40:04 +0100

    Removing more old code

--- src/app/_services/data.service.ts
+++ src/app/_services/data.service.ts
@@ -3,10 +3,8 @@ import axios from "axios";
 @Injectable()
 export class DataService {
   headers: any;
-  private roleUrl = "/api/roles";
   private departmentUrl = "/api/departments";
   private capabilityUrl = "/api/capabilities";
-  private role_capabilityUrl = "/api/role_capability";
   private getAllDataUrl = "/api/allData"
 
   constructor() {}
@@ -42,59 +40,9 @@ export class DataService {
       });
   }
 
-  getRoleByDepartment(departmentID: number) {
-    return axios
-      .get(this.roleUrl, {
-        params: {
-          departmentID: departmentID
-        },
-        headers: this.getHeaders()
-      })
-      .then(function(response) {
-        return response.data;
-      })
-      .catch(function(error) {
-        if (error.response) {
-          return {
-            error: error.response.data.Message,
-            statusCode: error.response.statusCode
-          };
-        } else {
-          if (error.message) {
-            return { error: error.message };
-          }
-        }
-      });
-  }
-
   getCapabilityNamesByDepartment(departmentID: number) {
     return axios
       .get(this.capabilityUrl, {
-        params: {
-          departmentID: departmentID
-        },
-        headers: this.getHeaders()
-      })
-      .then(function(response) {
-        return response.data;
-      })
-      .catch(function(error) {
-        if (error.response) {
-          return {
-            error: error.response.data.Message,
-            statusCode: error.response.statusCode
-          };
-        } else {
-          if (error.message) {
-            return { error: error.message };
-          }
-        }
-      });
-  }
-
-  getRoleByCapability(departmentID: number) {
-    return axios
-      .get(this.role_capabilityUrl, {
         params: {
           departmentID: departmentID
         },
