2f2821217227af530cbaff40d6e2341111f1fb0f
sapphireduffy <sapphireduffy@gmail.com>
2019-07-17 14:39:09 +0100

    changes made from pull request

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 const appRoutes: Routes = [
   {path:'home', component: LandingPageComponent},
   {path:'career', component: CareerTableComponent},
- // {path: '', redirectTo:'/home', pathMatch:'full'},
   { path: '**', redirectTo: '' }
 ];
 export const routing = RouterModule.forRoot(appRoutes);
