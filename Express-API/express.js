84841447ab39a28a25e2dd8240e70bd316326819
Ben Andrew <bena@Chalmers.local>
2019-07-18 12:52:19 +0100

    express update

--- Express-API/express.js
+++ Express-API/express.js
@@ -1,86 +1,72 @@
-const Database = require('./db.js')
-const LoginHandler = require('./login.js')
 const cors = require('cors')
 const express = require('express')
 var path = require('path')
 
-const app = express();
-const PORT = 8000;
+const app = express()
+const PORT = 8000
 
-app.use(express.static(path.join(__dirname, "public")));
-
-var db = new Database();
-var loginHandler = new LoginHandler();
-//loginHandler.createUser({Username : "test2", Password: "pass"}, db) 
+const Database = require('./db.js')
+const LoginHandler = require('./login.js')
+const db = new Database()
+const loginHandler = new LoginHandler()
 
 // Parse URL-encoded bodies (as sent by HTML forms)
-app.use(express.urlencoded());
+app.use(express.urlencoded())
 
 //Parse JSON bodies (as sent by API clients)
-app.use(express.json());
+app.use(express.json())
+
+const rolesQuery = "SELECT role_id, name, department_id, band_id FROM role WHERE department_id = ?"
+const capabilitiesQuery = "SELECT name, capability_id FROM capability WHERE department_id = ?"
+const departmentsQuery = "SELECT name FROM department WHERE department_id = ?"
+const rolesInDepQuery = "SELECT * FROM viewTableData WHERE department_id = ?"
+const bandsQuery = "SELECT * FROM band"
+const getRoleQuery = "SELECT role_id, name, summary, job_spec_url FROM role WHERE role_id = ?"
+
+const tokenCookieName = 'token'
 
 app.get("/roles", cors(), function(request, response) {
-  db.query(
-    "SELECT role_id, name, department_id, band_id FROM role WHERE department_id = ?",
-      [request.query.departmentID]
-  ).then(rows => {
+  db.query(rolesQuery,[request.query.departmentID]).then(rows => {
     response.send(rows);
   });
 });
 
 app.get("/capabilities", cors(), function(request, response) {
-  db.query(
-    "SELECT name, capability_id FROM capability WHERE department_id = ?",
-    [request.query.departmentID]
-  ).then(rows => {
+  db.query(capabilitiesQuery,[request.query.departmentID]).then(rows => {
     response.send(rows);
   });
 });
 
 app.get("/departments", cors(), function(request, response) {
-  db.query(
-    "SELECT name FROM department WHERE department_id = ?",
-      [request.query.departmentID]
-  ).then(rows => {
+  db.query(departmentsQuery,[request.query.departmentID]).then(rows => {
     response.send(rows);
   });
 });
 
 app.get("/rolesInDep", cors(), function(request, response) {
-  db.query(
-    "SELECT * FROM viewTableData WHERE department_id = ?",
-    [request.query.departmentID]
-  ).then(rows => {
+  db.query(rolesInDepQuery,[request.query.departmentID]).then(rows => {
     response.send(rows);
   });
 });
 
 app.get("/bands", cors(), function(request, response) {
-  db.query(
-    "SELECT * FROM band"
-  ).then(rows => {
+  db.query(bandsQuery).then(rows => {
     response.send(rows);
   });
 });
 
 app.post('/login', cors(), function (request, response) {
-	console.log(request.body);
-	console.log(request.body.Password)
 	loginHandler.login(request.body, db).then(token => {
-    //console.log(token)
-    response.cookie('token',token)
-		response.send({'token':token})
+    response.cookie(tokenCookieName,token)
+		response.send({tokenCookieName:token})
 	}, reason => {
-		//console.log(reason)
+		console.log(reason)
 		response.send(reason)
 	})
 });
 
 app.get("/role", cors(), function(request, response) {
-  db.query(
-    "SELECT role_id, name, summary, job_spec_url FROM role WHERE role_id = ?",
-      [request.query.roleID]
-  ).then(rows => {
+  db.query(getRoleQuery,[request.query.roleID]).then(rows => {
     response.send(rows);
   });
 });
