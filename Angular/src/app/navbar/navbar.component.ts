0934e95fa21af1919e0525cd351a339d0a898e18
Marc Templeton <marcte@nighty.local>
2019-07-22 13:29:33 +0100

    auth injected into nav for token

--- Angular/src/app/navbar/navbar.component.ts
+++ Angular/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthGuardComponent } from '../auth-guard/auth-guard.component';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authComponent: AuthGuardComponent) { }
 
   ngOnInit() {
   }
