5aa555e9f252f256c5803faa7bafdabb2f2b9ebb
Marc Templeton <marcte@nighty.local>
2019-07-22 16:34:48 +0100

    basic toggle for admin/emp added
    global way of attaining whether admin added to data service

--- Angular/src/app/navbar/navbar.component.ts
+++ Angular/src/app/navbar/navbar.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthGuardComponent } from '../auth-guard/auth-guard.component';
+import { DataService } from '../_services/data.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
+
 export class NavbarComponent implements OnInit {
 
-  constructor(private authComponent: AuthGuardComponent) { }
-
-  ngOnInit() {
+  constructor(private authComponent: AuthGuardComponent, private dataService: DataService) { 
+    if(this.authComponent.isAdmin())
+    {
+      this.dataService.isAdmin = true;
+    }
+    else
+    {
+      this.dataService.isAdmin = false;
+    }
   }
 
+  ngOnInit() { }
+
+  switchRoleToAdmin(switchToAdmin: boolean){
+    this.dataService.isAdmin = switchToAdmin
+  }
 }
