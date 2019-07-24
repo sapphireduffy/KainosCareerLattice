e378809dc481298195fd5b66a38048a5da2214c8
Marc Templeton <marcte@nighty.local>
2019-07-24 12:06:07 +0100

    admin-table create test working

--- Angular/src/app/admin-table/admin-table.component.spec.ts
+++ Angular/src/app/admin-table/admin-table.component.spec.ts
@@ -1,14 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminTableComponent } from './admin-table.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { DataService } from '../_services/data.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthGuardComponent } from '../auth-guard/auth-guard.component';
+import { AdminAuthGuardComponent } from '../admin-auth-guard/admin-auth-guard.component';
+import { CookieService } from 'ngx-cookie-service';
 
-describe('AdminTableComponent', () => {
+fdescribe('AdminTableComponent', () => {
   let component: AdminTableComponent;
   let fixture: ComponentFixture<AdminTableComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdminTableComponent ]
+      declarations: [ AdminTableComponent,
+                      NavbarComponent
+                    ],
+     imports: [ NgbAlertModule,
+                RouterTestingModule
+              ],
+     providers: [ AuthGuardComponent,
+                  AdminAuthGuardComponent,
+                  DataService,
+                  CookieService
+                ]
     })
     .compileComponents();
   }));
