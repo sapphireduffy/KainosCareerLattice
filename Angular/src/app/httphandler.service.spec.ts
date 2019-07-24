a1420351aaa7bc84bc9a4fca0bc7f501c8a052e4
Ben Andrew <bena@Chalmers.local>
2019-07-24 11:50:52 +0100

    httphandler service fix

--- Angular/src/app/httphandler.service.spec.ts
+++ Angular/src/app/httphandler.service.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HttphandlerService } from './httphandler.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('HttphandlerService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [ 
+      HttpHandler,
+      HttpClient
+    ]
+  }));
 
   it('should be created', () => {
     const service: HttphandlerService = TestBed.get(HttphandlerService);
