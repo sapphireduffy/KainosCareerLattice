5c218cb7e979ba083e004edc09f288acf741c6e5
Marc Templeton <marcte@nighty.local>
2019-07-24 11:49:50 +0100

    add-capability create test working

--- Angular/src/app/add-capability/add-capability.component.spec.ts
+++ Angular/src/app/add-capability/add-capability.component.spec.ts
@@ -1,14 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddCapabilityComponent } from './add-capability.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { DataService } from '../_services/data.service';
 
-describe('AddCapabilityComponent', () => {
+fdescribe('AddCapabilityComponent', () => {
   let component: AddCapabilityComponent;
   let fixture: ComponentFixture<AddCapabilityComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddCapabilityComponent ]
+      declarations: [ AddCapabilityComponent ],
+      imports: [ ReactiveFormsModule ],
+      providers: [ NgbActiveModal,
+                   DataService 
+                 ]
     })
     .compileComponents();
   }));
