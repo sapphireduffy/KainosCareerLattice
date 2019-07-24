d0de7c5b7ea130d0a13ac84ed826d3031c87a449
Ben Andrew <bena@Chalmers.local>
2019-07-24 12:01:27 +0100

    fixed app component

--- Angular/src/app/app.component.spec.ts
+++ Angular/src/app/app.component.spec.ts
@@ -1,10 +1,17 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterOutlet, ChildrenOutletContexts } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      providers:[
+        ChildrenOutletContexts
+      ],
       declarations: [
+        RouterOutlet,
+        NavbarComponent,
         AppComponent
       ],
     }).compileComponents();
