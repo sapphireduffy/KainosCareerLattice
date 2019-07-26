01e966083c9c1d6757800bc0910c53aced62df67
Ben Andrew <bena@Chalmers.local>
2019-07-26 10:49:39 +0100

    backdrop static{

--- Angular/src/app/services/modal.service.ts
+++ Angular/src/app/services/modal.service.ts
@@ -54,7 +54,7 @@ export class ModalService {
 
   openAddCapabilityModal(departmentId) {
     return new Promise((resolve, reject) => {
-      const modalRef = this.modalService.open(AddCapabilityComponent);
+      const modalRef = this.modalService.open(AddCapabilityComponent, { backdrop: "static" });
       modalRef.componentInstance.departmentId = departmentId;
       modalRef.componentInstance.capabilityAdded.subscribe(data => {
         resolve(data)
@@ -64,7 +64,7 @@ export class ModalService {
 
   openAddBandModal(departmentId, aboveValue, belowValue, schoolId) {
     return new Promise((resolve, reject) => {
-      const modalRef = this.modalService.open(AddBandComponent);
+      const modalRef = this.modalService.open(AddBandComponent, { backdrop: "static" });
       modalRef.componentInstance.schoolId = schoolId;
       modalRef.componentInstance.abovePriorityVal = aboveValue;
       modalRef.componentInstance.belowPriorityVal = belowValue;
@@ -78,7 +78,7 @@ export class ModalService {
   openEditRoleModal(roleId, capabilities, bands) {
     return new Promise((resolve, reject) => {
       this.dataService.getRole(roleId).then(response => {
-        const modalRef = this.modalService.open(EditRoleModalComponent);
+        const modalRef = this.modalService.open(EditRoleModalComponent, { backdrop: "static" });
         modalRef.componentInstance.roleToEdit = response[0];
         modalRef.componentInstance.capabilities = capabilities;
         modalRef.componentInstance.bands = bands;
